ae42b241556d70d525fffcd2c0b6f07611873941
Kubilay Kaya <kkubilay24@gmail.com>
2021-07-24 00:09:11 +0300

    initial
    initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,7 @@
 import "../styles/globals.scss";
-import { User } from "../context/UserContext";
-import { useEffect, useState } from "react";
-import Redirect from "../components/Redirect";
 
 function MyApp({ Component, pageProps }) {
-  const [isAuth, setIsAuth] = useState(false);
-
-  useEffect(() => {
-    setIsAuth(sessionStorage.getItem("user") ? true : false);
-  }, []);
-
-  return (
-    <User.Provider value={{ isAuth, setIsAuth }}>
-      <Component {...pageProps} />
-    </User.Provider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
